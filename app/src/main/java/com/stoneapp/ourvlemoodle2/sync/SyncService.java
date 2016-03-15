57af0d3cc4080165194f9fa0367306ad01c8a22d
Romario Maxwell <romario.maxwell@gmail.com>
2016-03-15 04:14:43 -0500

    Remove unnecessary comments copied from online guide.

--- app/src/main/java/com/stoneapp/ourvlemoodle2/sync/SyncService.java
+++ app/src/main/java/com/stoneapp/ourvlemoodle2/sync/SyncService.java
@@ -31,13 +31,9 @@ import com.stoneapp.ourvlemoodle2.sync.SyncAdapter;
  * onPerformSync().
  */
 public class SyncService extends Service {
-    // Storage for an instance of the sync adapter
     private static SyncAdapter sSyncAdapter = null;
-    // Object to use as a thread-safe lock
     private static final Object sSyncAdapterLock = new Object();
-    /*
-     * Instantiate the sync adapter object.
-     */
+    
     @Override
     public void onCreate() {
         /*
@@ -50,19 +46,9 @@ public class SyncService extends Service {
                 sSyncAdapter = new SyncAdapter(getApplicationContext(), true);
         }
     }
-    /**
-     * Return an object that allows the system to invoke
-     * the sync adapter.
-     *
-     */
+
     @Override
     public IBinder onBind(Intent intent) {
-        /*
-         * Get the object that allows external processes
-         * to call onPerformSync(). The object is created
-         * in the base class code when the SyncAdapter
-         * constructors call super()
-         */
         return sSyncAdapter.getSyncAdapterBinder();
     }
 }
